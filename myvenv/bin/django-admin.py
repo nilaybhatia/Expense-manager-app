ed2fb08f26e0b4284fefd6c47a011a8fceb92a4a
nilaybhatia <bhatianilay@gmail.com>
2020-08-15 21:22:03 +0530

    Move gitignore

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/data/data/com.termux/files/home/Learning-Phase-Tasks/myvenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
